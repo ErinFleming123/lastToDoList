5759f4124dd83e1b97b14491d64cc56bcab74443
ErinFleming <flemingerin@hotmail.com>
2020-06-15 21:31:59 -0600

    eleventh commit yee haw

--- script.js
+++ script.js
@@ -1,10 +1,13 @@
 const listsContainer = document.querySelector('[data-lists]')
 const newListForm = document.querySelector('[data-new-list-form]')
 const newListInput = document.querySelector('[data-new-list-input]')
+const deleteListButton = document.querySelector('[data-delete-list-button]')
+const listDisplayContainer = document.querySelector('[data-list-display-container]')
+const listTitleElement
 
 const LOCAL_STORAGE_LIST_KEY = 'task.lists'
-const LOCAL_STORAGE_SELECTED_LIST_KEY = 'task.selectedListId'
-let lists = JSON.parse(localStorage.getItem(LOCAL_STORAGE_LIST_KEY) || [])
+const LOCAL_STORAGE_SELECTED_LIST_ID_KEY = 'task.selectedListId'
+let lists = JSON.parse(localStorage.getItem(LOCAL_STORAGE_LIST_KEY)) || []
 let selectedListId = localStorage.getItem(LOCAL_STORAGE_SELECTED_LIST_ID_KEY)
 
 listsContainer.addEventListener('click', e=> {
@@ -12,6 +15,12 @@ listsContainer.addEventListener('click', e=> {
     selectedListId = e.target.dataset.listId
     saveAndRender()
   }
+})
+
+deleteListButton.addEventListener('click', e => {
+  lists = lists.filter(list => list.id !== selectedListId)
+  selectedListId = null
+  saveAndRender()
 })
 
 newListForm.addEventListener('submit', e => {
