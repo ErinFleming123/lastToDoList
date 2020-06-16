cdd17f96336146a40fce74fbf5e5204e9d1cd59c
ErinFleming <flemingerin@hotmail.com>
2020-06-15 19:56:34 -0600

    ninth commit

--- script.js
+++ script.js
@@ -3,7 +3,11 @@ const newListForm = document.querySelector('[data-new-list-form]')
 const newListInput = document.querySelector('[data-new-list-input]')
 
 const LOCAL_STORAGE_LIST_KEY = 'task.lists'
+const LOCAL_STORAGE_SELECTED_LIST_KEY = 'task.selectedListId'
 let lists = JSON.parse(localStorage.getItem(LOCAL_STORAGE_LIST_KEY) || [])
+let selectedListId = localStorage.getItem(LOCAL_STORAGE_SELECTED_LIST_ID_KEY)
+
+listsContainer.addEventListener
 
 newListForm.addEventListener('submit', e => {
   e.preventDefault()
@@ -36,6 +40,9 @@ function render() {
     listElement.dataset.listId = list.id
     listElement.classList.add("list-name")
     listElement.innerText = list.name
+    if (list.id === selectedListId) {
+      listElement.classList.add('active-list')
+    }
     listsContainer.appendChild(listElement)
   })
 }
